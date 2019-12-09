c68607efb94f42142991e608a4266e341b7a1d2d
mysteriousdog <3076777685@qq.com>
2019-12-09 15:38:27 +0800

    leetcode 48 简单的举证旋转 n2的时间复杂度

--- main.cpp
+++ main.cpp
@@ -14,11 +14,7 @@ int main(){
 
   
   Solution *s = new Solution();
-  vector<int> nums;
-  nums.push_back(1);
-  nums.push_back(2);
-  nums.push_back(3);
-  vector<vector<int> > vec = s->permute(nums);
+  
   for (int i = 0; i < vec.size(); i++)
   {
     vector<int> vv = vec[i];
