f0ce2dfc2fb197a96adebb7deee660c443115487
mysteriousdog <3076777685@qq.com>
2020-01-10 12:37:36 +0800

    leetcode 82 done

--- main.cpp
+++ main.cpp
@@ -7,14 +7,10 @@
 
 int main(){
 
-  int target = 8;
-  vector<int> v = Tool::buildVec();
+  ListNode* head = Tool::buildList();
   Solution *s = new Solution();
-  vector<vector<int> >vec = s->combinationSum(v,target);
-  cout<<"len "<<vec.size()<<endl;
-  for(int i =0;i<vec.size();i++){
-    Tool::showVec(vec[i]);
-  }
+  head = s->deleteDuplicates(head);
+  Tool::showList(head);
   return 0;
 }
   
